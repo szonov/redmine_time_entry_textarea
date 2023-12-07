6a68822794620d863ab1c06c7ef8b4c50bbdea4a
Sergey Zonov <sergey@zonov.com>
2023-12-07 16:57:19 +0700

    compatibility with redmine 5.1.1

--- init.rb
+++ init.rb
@@ -1,14 +1,14 @@
 require 'redmine'
 
-require_dependency 'redmine_time_entry_textarea/hooks'
+require_relative './lib/redmine_time_entry_textarea/hooks.rb'
 
 Redmine::Plugin.register :redmine_time_entry_textarea do
   name 'Record time entry in textarea'
   author 'Sergey Zonov'
   description 'This plugins replace input box with textarea for time entries.'
-  version '1.0.0'
+  version '5.1.1'
   url 'https://github.com/szonov/redmine_time_entry_textarea'
   author_url 'https://github.com/szonov'
-  requires_redmine :version_or_higher => '4.2.2'
+  requires_redmine :version_or_higher => '5.1.1'
 end
 
